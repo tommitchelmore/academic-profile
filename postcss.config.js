94905a76e2d148a799c566a5d233edc9a5746781
Thomas Mitchelmore <tommitchelmore@outlook.com>
2021-02-05 14:10:04 +0000

    Added source label

--- postcss.config.js
+++ postcss.config.js
@@ -5,10 +5,10 @@ module.exports = {
         require('tailwindcss'),
         require('autoprefixer'),
         require('postcss-import'),
-        //require('@fullhuman/postcss-purgecss')({
-        //    content: ["./index.html"],
-        //    defaultExtractor: (c) => c.match(/[A-Za-z0-9-_:/]+/g) || [],
-        //}),
+        require('@fullhuman/postcss-purgecss')({
+            content: ["./index.html"],
+            defaultExtractor: (c) => c.match(/[A-Za-z0-9-_:/]+/g) || [],
+        }),
         require('cssnano')
     ]
   }
