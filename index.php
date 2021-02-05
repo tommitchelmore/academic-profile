ebfc043638326c743fa3e0d7cda69f1629bebe96
Thomas Mitchelmore <tommitchelmore@outlook.com>
2021-02-05 13:11:14 +0000

    First upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+  <meta charset="utf-8">
+
+  <title>The HTML5 Herald</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+
+  <link rel="stylesheet" href="resources/build.css">
+
+</head>
+
+<body>
+  <main class="flex justify-center items-center min-h-screen bg-gradient-to-br from-light-100 to-light-500 dark:from-dark-100 dark:to-dark-500">
+      <div class="w-full sm:w-2/3 lg:w-1/3 2xl:w-1/4 rounded-3xl" id="card">
+        <div class="h-64 bg-center bg-cover rounded-t-3xl" style="background-image: url('resources/profile.gif');"></div>
+        <div class="p-8">
+            <div class="pb-4">
+                <h1 class="text-2xl font-heading text-dark-500 dark:text-light-500">Thomas Mitchelmore</h1>
+                <p class="text-dark:100 dark:text-light-100">BSc Computer Science</p>
+            </div>
+            <div class="pb-4">
+                
+                <p class="text-xl font-heading text-dark-500 dark:text-light-500">Latest from Github:</p>
+
+            </div>
+            <div>
+                <div class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-link"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path></svg>
+                    <a href="https://tommitchelmore.com" target="_blank" ref="noopener noreferrer">tommitchelmore.com</a>
+                </div>
+                <div class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
+                    <a href="mailto:tm7g20@southampton.ac.uk" target="_blank" ref="noopener noreferrer">tm7g20@southampton.ac.uk</a>
+                </div>
+                <div class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
+                    <a href="mailto:tommitchelmore@outlook.com" target="_blank" ref="noopener noreferrer">tommitchelmore@outlook.com</a>
+                </div>
+                <div class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
+                    <a href="https://linkedin.com/in/tommitchelmore" target="_blank" ref="noopener noreferrer">/in/tommitchelmore</a>
+                </div>
+            </div>
+        </div>
+      </div>
+  </main>
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  //purge: [
+  //  './**/*.html'
+  //],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'heading': ['Poppins', 'sans-serif'],
+      'body': ['Roboto', 'sans-serif']
+    },
+    colors: {
+      light: {
+        100: "#bdbdbd",
+        500: "#f0f0f0"
+      },
+      dark: {
+        100: "#303030",
+        500: "#101214"
+      },
+      blue: "#2872E2"
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
